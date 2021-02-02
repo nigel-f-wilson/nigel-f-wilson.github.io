9b30801fe26b103461fdf782e82fe55ae957e124
nolastemgarden <nolastemgarden@gmail.com>
2021-02-01 20:43:11 -0600

    Fix CardMedia props & tidy up imports

--- src/components/SquarePhotoCard.js
+++ src/components/SquarePhotoCard.js
@@ -1,25 +1,8 @@
 import React from 'react';
 
-import clsx from 'clsx';
-import { spacing } from '@material-ui/system';
-
-
-// MY components
-import Navbar from "../components/Navbar/Navbar";
-
-
 // MUI CORE Imports
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
@@ -39,7 +22,7 @@ export default function SquarePhotoCard(props) {
             <CardMedia
                 className={classes.cardImage}
                 image={props.img}
-                // title={props.title}
+                title={props.title}
             />
         </Card>
     );

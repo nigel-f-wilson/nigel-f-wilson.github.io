c9bc6ffdb1a35ee66a7de3e4a20216ba818eb5b3
nolastemgarden <nolastemgarden@gmail.com>
2021-02-07 21:30:49 -0600

    Auto update of import path

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import PuzzleCollectionPage from "./pages/Teach/PuzzleCollection"
 
 import GardenPage from "./pages/Garden/Garden.js";
 import AboutMePage from "./pages/AboutMe";
-import ContactPage from "./pages/Contact";
+import ContactPage from "./pages/Contact/Contact";
 
 //  MUI imports
 import CssBaseline from '@material-ui/core/CssBaseline';
